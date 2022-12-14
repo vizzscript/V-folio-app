c62c4b38020bd71d169b50c59d0a2bb6cddb72da
CodeInsideU <83591308+CodeInsideU@users.noreply.github.com>
2022-12-14 10:28:02 +0530

    updated

--- client/src/components/Skills.jsx
+++ client/src/components/Skills.jsx
@@ -17,67 +17,78 @@ const skills = () => {
             id:1,
             src: html,
             title: 'HTML',
-            style: 'shadow-orange-400'
+            style: 'shadow-orange-400',
+            href: 'https://developer.mozilla.org/en-US/docs/Web/HTML'
         },
         {
             id:2,
             src: css,
             title: 'CSS',
-            style: 'shadow-blue-400'
+            style: 'shadow-blue-400',
+            href: 'https://developer.mozilla.org/en-US/docs/Web/CSS'
         },
         {
             id:3,
             src: javascript,
             title: 'Javascript',
-            style: 'shadow-yellow-400'
+            style: 'shadow-yellow-400',
+            href: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript'
         },
         {
             id:4,
             src: bootstrap,
             title: 'Bootstrap',
-            style: 'shadow-fuchsia-400'
+            style: 'shadow-fuchsia-400',
+            href: 'https://getbootstrap.com/'
         },
         {
             id:5,
             src: tailwind,
             title: 'Tailwind',
-            style: 'shadow-blue-400'
+            style: 'shadow-blue-400',
+            href: 'https://tailwindcss.com/'
         },
         {
             id:6,
             src: mongodb,
             title: 'MongoDB',
-            style: 'shadow-green-400'
+            style: 'shadow-green-400',
+            href: 'https://www.mongodb.com/home'
         },
         {
             id:7,
             src: reaction,
             title: 'React',
-            style: 'shadow-blue-400'
+            style: 'shadow-blue-400',
+            href: 'https://reactjs.org/'
         },
         {
             id:8,
             src: node,
             title: 'Node',
-            style: 'shadow-green-400'
+            style: 'shadow-green-400',
+            href: 'https://nodejs.org/en/'
         },
         {
             id:9,
             src: docker,
             title: 'Docker',
-            style: 'shadow-blue-400'
+            style: 'shadow-blue-400',
+            href: 'https://www.docker.com/'
         },
         {
             id:10,
             src: python,
             title: 'Python',
-            style: 'shadow-yellow-400'
+            style: 'shadow-yellow-400',
+            href: 'https://www.python.org/'
         },
         {
             id:11,
             src: github,
             title: 'Github',
-            style: 'shadow-fuchsia-400'
+            style: 'shadow-fuchsia-400',
+            href: 'https://github.com/'
         }
     ]
 
@@ -91,12 +102,14 @@ const skills = () => {
                 </div>
 
                 <div className='w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0 text-gray-400 font-bold'>
-                    {skills.map(({id,src,title,style}) => (
+                    {skills.map(({id,src,title,style,href}) => (
                         <div 
                         key={id}
-                        className={`shadow-md hover:scale-105 duration-500 py-2 rounded-lg ${style}`}>
-                        <img src={src} alt="" className='w-20 mx-auto'/>
-                        <p className='mt-4'>{title}</p>
+                        className={`shadow-md hover:scale-105 hover:text-white duration-500 py-2 rounded-lg ${style}`}>
+                        <a href={href}>
+                            <img src={src} alt="" className='w-20 mx-auto'/>
+                            <p className='mt-4'>{title}</p>
+                            </a>
                     </div>
                     ))}
                     
